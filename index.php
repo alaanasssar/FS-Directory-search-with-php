0be1fe67bda3e802b69bb2f80e8ed3a4e4f10804
Alaa nassar <alaaanasssar@gmail.com>
2017-06-11 13:21:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+echo '<pre>';
+function txt_search( $string, $dir ) {
+     foreach ( new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as $f ) {
+        if(is_file($f->getPathname())){
+             $content = file($f->getPathname());
+            foreach($content as  $key => $line){
+                if (stripos($line, $string) !== false) {
+                    $file_name = $f->getPathname();
+                    $file_name = str_replace('.\\', '', $file_name);
+                  echo 'File name: '.$file_name; 
+                   $line = str_replace($string,'<b style="color: red;">'.$string.'</b>',$line);
+                   $line = str_replace('  ','',$line);
+                   echo "<br />[".$key."] => " .$line. "<br /><hr />";
+                   
+                }
+            }
+          
+        }
+     }
+}
+txt_search('something','./');
+echo '</pre>';
